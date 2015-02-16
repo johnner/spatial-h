970d6d49da643be5d51078c490d0622c7e1ef5bf
Anton Zhusman <zhusman@allyst.com>
2015-02-16 17:31:56 +0300

    add test

---
+++ test/spatial.test.js
@@ -0,0 +1,9 @@
+
+describe('retrieve', function () {
+	var spatial = new SpatialHash();
+	beforeEach(function () {});
+
+	it("should be empty hash object", function () {
+		expect(spatial.buckets).toBeDefined();
+	});
+});
